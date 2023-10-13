73cff16d6ffcd51222d80fa6e5a8462c412ff854
Stefan Traub <traub@hs-ulm.de>
2023-10-13 16:15:53 +0200

    first example

---
+++ socket1/client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Socket s = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        s.Connect("127.0.0.1", 12345);
+        StreamWriter wr = new StreamWriter(new NetworkStream(s));
+        wr.WriteLine("hello from client");
+        wr.Flush();
+        s.Close();
+    }
+}
---
+++ socket1/server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Socket s = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPAddress adr =  IPAddress.Any;
+        IPEndPoint p = new(adr, 12345);
+        s.Bind(p);
+        s.Listen();
+        Socket cl = s.Accept();
+
+        StreamReader rd = new StreamReader(new NetworkStream(cl));
+        string? data = rd.ReadLine();
+        System.Console.WriteLine(data);
+        
+        s.Close();
+    }
+}
