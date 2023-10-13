18af17b0e8bab4ecde374621e3e222f6f68c973e
Stefan Traub <traub@hs-ulm.de>
2023-10-13 16:33:31 +0200

    udp

---
+++ socket2/client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Socket s = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        IPAddress adr =  IPAddress.Parse("127.0.0.1");
+        IPEndPoint p = new(adr, 12345);
+
+        string m = "hello from UDP";
+        byte[] data = Encoding.ASCII.GetBytes(m);
+        s.SendTo(data, p);
+    }
+}
