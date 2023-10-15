0643e686e32107ded5c00f8fb406211a5d239188
Stefan Traub <traub@hs-ulm.de>
2023-10-15 13:53:08 +0200

    Linux start scripts

--- socket/tcp/python/server/server.py
+++ socket/tcp/python/server/server.py
@@ -1,6 +1,7 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server_address = ('localhost', 12345)
 server_socket.bind(server_address)
@@ -14,3 +15,4 @@ data = connection.recv(1024)
 print(data.decode('utf-8'))
 
 connection.close()
+server_socket.close()
--- socket/udp/python/server/server.py
+++ socket/udp/python/server/server.py
@@ -5,7 +5,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('localhost', 12345)
 server_socket.bind(server_address)
 
-print("UDP python server: waiting for packats")
+print("UDP python server: waiting for packets")
 
 data = server_socket.recv(1024)
 print(data.decode('utf-8'))
